b26cfb8ebb892de82615ded1ee54882a9cc04539
Matt Conway <mattconway727@gmail.com>
2019-04-12 21:16:39 -0500

    first two events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-// initial commit // Your code goes here
+// 1. (click) Change the background color on all 3 of the sign up buttons when clicked.
+const btn = document.querySelectorAll(".btn");
+
+btn.forEach(element => {
+  element.addEventListener("click", function(e) {
+    e.target.style.backgroundColor = "black";
+  });
+});
+
+// 2. (click) Make the Fun Bus h1 disappear.
+const logoDisappear = document.querySelector("h1");
+
+logoDisappear.addEventListener("click", function(e) {
+  e.target.style.display = "none";
+});
+
+// 3. (mouseover)
+
+// 4. (keydown)
+
+// 5. (wheel)
+
+// 6. (dblclick)
+
+// 7. (drag/drop)
+
+// 8. (focus)
+
+// 9. (scroll)
+
+// 10. (select)
+
+// Use stopProgagation() and preventDefault()
